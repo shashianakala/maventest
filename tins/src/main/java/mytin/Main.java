952a5637af833c15d6d65f7923177aa426a36297
shashianakala <shashianakala@gmail.com>
2017-10-17 06:58:19 +0100

    pushed into git

---
+++ tins/src/main/java/mytin/Main.java
@@ -0,0 +1,101 @@
+package mytin;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.strategicgains.restexpress.Format;
+import com.strategicgains.restexpress.Parameters;
+import com.strategicgains.restexpress.RestExpress;
+import com.strategicgains.restexpress.exception.BadRequestException;
+import com.strategicgains.restexpress.pipeline.SimpleConsoleLogMessageObserver;
+import com.strategicgains.restexpress.plugin.cache.CacheControlPlugin;
+import com.strategicgains.restexpress.plugin.metrics.MetricsPlugin;
+import com.strategicgains.restexpress.plugin.route.RoutesMetadataPlugin;
+import mytin.config.Configuration;
+import mytin.config.MetricsConfig;
+import mytin.serialization.ResponseProcessors;
+import com.strategicgains.restexpress.util.Environment;
+import com.strategicgains.syntaxe.ValidationException;
+import com.yammer.metrics.reporting.GraphiteReporter;
+
+public class Main
+{
+	private static final String SERVICE_NAME = "TODO: Enter Service Name";
+	private static final Logger LOG = LoggerFactory.getLogger(SERVICE_NAME);
+
+	public static void main(String[] args) throws Exception
+	{
+		Configuration config = loadEnvironment(args);
+		RestExpress server = new RestExpress()
+		    .setName(SERVICE_NAME)
+		    .setBaseUrl(config.getBaseUrl())
+		    .setDefaultFormat(config.getDefaultFormat())
+		    .setExecutorThreadCount(config.getExecutorThreadPoolSize())
+		    .putResponseProcessor(Format.JSON, ResponseProcessors.json())
+		    .putResponseProcessor(Format.XML, ResponseProcessors.xml())
+		    .putResponseProcessor(Format.WRAPPED_JSON, ResponseProcessors.wrappedJson())
+		    .putResponseProcessor(Format.WRAPPED_XML, ResponseProcessors.wrappedXml())
+		    .addMessageObserver(new SimpleConsoleLogMessageObserver());
+
+		Routes.define(config, server);
+		configureMetrics(config, server);
+
+		new RoutesMetadataPlugin()							// Support basic discoverability.
+			.register(server)
+			.parameter(Parameters.Cache.MAX_AGE, 86400);	// Cache for 1 day (24 hours).
+
+		new CacheControlPlugin()							// Support caching headers.
+			.register(server);
+
+		mapExceptions(server);
+		server.bind(config.getPort());
+		server.awaitShutdown();
+	}
+
+	private static void configureMetrics(Configuration config, RestExpress server)
+    {
+		MetricsConfig mc = config.getMetricsConfig();
+
+	    if (mc.isEnabled())
+		{
+			new MetricsPlugin()
+				.virtualMachineId(mc.getMachineName())
+				.register(server);
+
+			if (mc.isGraphiteEnabled())
+			{
+				GraphiteReporter.enable(mc.getPublishSeconds(), TimeUnit.SECONDS,
+					mc.getGraphiteHost(), mc.getGraphitePort());
+			}
+			else
+			{
+				LOG.warn("*** Graphite Metrics Publishing is Disabled ***");
+			}
+		}
+		else
+		{
+			LOG.warn("*** Metrics Generation is Disabled ***");
+		}
+    }
+
+    private static void mapExceptions(RestExpress server)
+    {
+    	server
+	    	.mapException(ValidationException.class, BadRequestException.class);
+    }
+
+	private static Configuration loadEnvironment(String[] args)
+    throws FileNotFoundException, IOException
+    {
+	    if (args.length > 0)
+		{
+			return Environment.from(args[0], Configuration.class);
+		}
+
+	    return Environment.fromDefault(Configuration.class);
+    }
+}
---
+++ tins/src/main/java/mytin/config/MetricsConfig.java
@@ -0,0 +1,98 @@
+package mytin.config;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.strategicgains.restexpress.exception.ConfigurationException;
+
+public class MetricsConfig
+{
+	private static final Logger LOG = LoggerFactory.getLogger(MetricsConfig.class);
+	private static final String METRICS_IS_ENABLED_PROPERTY = "metrics.isEnabled";
+	private static final String METRICS_MACHINE_NAME_PROPERTY = "metrics.machineName";
+	private static final String GRAPHITE_IS_ENABLED_PROPERTY = "metrics.graphite.isEnabled";
+	private static final String GRAPHITE_HOST_PROPERTY = "metrics.graphite.host";
+	private static final String GRAPHITE_PORT_PROPERTY = "metrics.graphite.port";
+	private static final String GRAPHITE_PUBLISHING_SECONDS_PROPERTY = "metrics.graphite.publishSeconds";
+
+	private boolean isEnabled;
+	private String machineName;
+	private boolean isGraphiteEnabled;
+	private String graphiteHost;
+	private Integer graphitePort;
+	private Integer publishSeconds;
+
+	public MetricsConfig(Properties p)
+	{
+		isEnabled = Boolean.parseBoolean(p.getProperty(METRICS_IS_ENABLED_PROPERTY, "true"));
+		if (!isEnabled) return;
+
+		isGraphiteEnabled = Boolean.parseBoolean(p.getProperty(GRAPHITE_IS_ENABLED_PROPERTY, "true"));
+		if (!isGraphiteEnabled) return;
+
+		machineName = p.getProperty(METRICS_MACHINE_NAME_PROPERTY);
+		
+		if (machineName == null)
+		{
+			LOG.warn("*** Machine Name (" + METRICS_MACHINE_NAME_PROPERTY + ") is not set. ***");
+		}
+
+		graphiteHost = p.getProperty(GRAPHITE_HOST_PROPERTY);
+
+		if (graphiteHost == null)
+		{
+			throw new ConfigurationException("Please define a graphite host for property: " + GRAPHITE_HOST_PROPERTY);
+		}
+
+		try
+		{
+			graphitePort = Integer.parseInt(p.getProperty(GRAPHITE_PORT_PROPERTY));
+		}
+		catch (NumberFormatException e)
+		{
+			throw new ConfigurationException("Please define a graphite port for property: " + GRAPHITE_PORT_PROPERTY, e);
+		}
+
+		try
+		{
+			publishSeconds = Integer.parseInt(p.getProperty(GRAPHITE_PUBLISHING_SECONDS_PROPERTY));
+		}
+		catch (NumberFormatException e)
+		{
+			throw new ConfigurationException("Please define how frequently (in seconds) to publish to graphite in property: "
+			        + GRAPHITE_PUBLISHING_SECONDS_PROPERTY, e);
+		}
+	}
+
+	public boolean isEnabled()
+	{
+		return isEnabled;
+	}
+	
+	public String getMachineName()
+	{
+		return machineName;
+	}
+
+	public boolean isGraphiteEnabled()
+	{
+		return isGraphiteEnabled;
+	}
+
+	public String getGraphiteHost()
+	{
+		return graphiteHost;
+	}
+
+	public Integer getGraphitePort()
+	{
+		return graphitePort;
+	}
+
+	public Integer getPublishSeconds()
+	{
+		return publishSeconds;
+	}
+}
